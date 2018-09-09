953393727372844d36aa2dc162f00a17d1f13878
Arnold Galovics <galovicsarnold@gmail.com>
2018-09-09 18:24:37 +0200

    Added input handling for twitter account

--- src/admin/popup_settings.php
+++ src/admin/popup_settings.php
@@ -28,7 +28,19 @@ final class SocialMediaEverywherePopupSettings implements Settings
 
     public function registerSettings()
     {
-        add_option(SME_TWITTER_ACCOUNT, '');
-        register_setting(SME_OPTIONS_GROUP, SME_TWITTER_ACCOUNT);
+        add_option(SME_TWITTER_ACCOUNT, 'https://');
+        register_setting(SME_OPTIONS_GROUP, SME_TWITTER_ACCOUNT, array(
+            'sanitize_callback' => array($this, 'handleTwitterAccountChange')
+        ));
+    }
+
+    public function handleTwitterAccountChange($value) {
+        if (empty($value)) {
+            return 'https://';
+        }
+        if (substr($value, 0, 4) !== 'http') {
+            return 'https://' . $value;
+        }
+        return $value;
     }
 }
