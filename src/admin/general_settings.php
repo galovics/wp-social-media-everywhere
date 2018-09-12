3367a6ad7e0f388840ef98ab429723af478032de
Arnold Galovics <galovicsarnold@gmail.com>
2018-09-12 21:56:07 +0200

    Moved account settings under general settings page

--- src/admin/general_settings.php
+++ src/admin/general_settings.php
@@ -24,10 +24,23 @@ final class SocialMediaEverywhereGeneralSettings implements Settings
     public function render()
     {
     ?>
-
-<div>
-	<label for="<?php echo SME_ENABLED; ?>">Enabled</label>
-	<input type="checkbox" id="<?php echo SME_ENABLED; ?>" name="<?php echo SME_ENABLED; ?>" value="1" <?php checked(get_option(SME_ENABLED), 1); ?>/>
+<div id="sme-general-settings" class="settings">
+    <div id="sme-enabled-settings" class="settings-section">
+        <div class="setting-row">
+            <label for="<?php echo SME_ENABLED; ?>">Enabled</label>
+            <input type="checkbox" id="<?php echo SME_ENABLED; ?>" name="<?php echo SME_ENABLED; ?>" value="1" <?php checked(get_option(SME_ENABLED), 1); ?>/>
+        </div>
+    </div>
+    <div id="sme-account-settings" class="settings-section">
+        <div class="account-twitter setting-row">
+            <label for="<?php echo SME_TWITTER_ACCOUNT; ?>">Twitter account</label>
+            <input type="text" id="<?php echo SME_TWITTER_ACCOUNT; ?>" name="<?php echo SME_TWITTER_ACCOUNT; ?>" value="<?php echo get_option(SME_TWITTER_ACCOUNT); ?>" />
+        </div>
+        <div class="account-linkedin setting-row">
+            <label for="<?php echo SME_LINKEDIN_ACCOUNT; ?>">LinkedIn account</label>
+            <input type="text" id="<?php echo SME_LINKEDIN_ACCOUNT; ?>" name="<?php echo SME_LINKEDIN_ACCOUNT; ?>" value="<?php echo get_option(SME_LINKEDIN_ACCOUNT); ?>" />
+        </div>
+    </div>
 </div>
 
 <?php
@@ -39,5 +52,32 @@ final class SocialMediaEverywhereGeneralSettings implements Settings
         register_setting(SME_OPTIONS_GROUP, SME_ENABLED, array(
             'sanitize_callback' => array($this, 'handleEnabledChange')
         ));
+        
+        add_option(SME_TWITTER_ACCOUNT, 'https://');
+        register_setting(SME_OPTIONS_GROUP, SME_TWITTER_ACCOUNT, array(
+            'sanitize_callback' => array($this, 'handleTwitterAccountChange')
+        ));
+        add_option(SME_LINKEDIN_ACCOUNT, 'https://');
+        register_setting(SME_OPTIONS_GROUP, SME_LINKEDIN_ACCOUNT, array(
+            'sanitize_callback' => array($this, 'handleLinkedInAccountChange')
+        ));
+    }
+
+    private function sanitizeURL($value) {
+        if (empty($value)) {
+            return $value;
+        }
+        if (substr($value, 0, 4) !== 'http') {
+            return 'https://' . $value;
+        }
+        return $value;
+    }
+
+    public function handleTwitterAccountChange($value) {
+        return $this->sanitizeURL($value);
+    }
+
+    public function handleLinkedInAccountChange($value) {
+        return $this->sanitizeURL($value);
     }
 }
--- src/admin/popup_settings.php
+++ src/admin/popup_settings.php
@@ -17,16 +17,14 @@ final class SocialMediaEverywherePopupSettings implements Settings
     public function render()
     {
     ?>
-<div class="popup-settings">
-    <div class="popup-show-settings">
-        <input type="radio" name="<?php echo SME_POPUP_SHOW_SETTING; ?>" value="0" <?php checked(get_option(SME_POPUP_SHOW_SETTING), 0); ?>>Disabled<br>
-        <input type="radio" name="<?php echo SME_POPUP_SHOW_SETTING; ?>" value="1" <?php checked(get_option(SME_POPUP_SHOW_SETTING), 1); ?>>At the end of the posts
-    </div>
-    <div class="account-settings">
-        <label for="<?php echo SME_TWITTER_ACCOUNT; ?>">Twitter account</label>
-        <input type="text" id="<?php echo SME_TWITTER_ACCOUNT; ?>" name="<?php echo SME_TWITTER_ACCOUNT; ?>" value="<?php echo get_option(SME_TWITTER_ACCOUNT); ?>" />
-        <label for="<?php echo SME_LINKEDIN_ACCOUNT; ?>">Twitter account</label>
-        <input type="text" id="<?php echo SME_LINKEDIN_ACCOUNT; ?>" name="<?php echo SME_LINKEDIN_ACCOUNT; ?>" value="<?php echo get_option(SME_LINKEDIN_ACCOUNT); ?>" />
+<div id="sme-popup-settings" class="settings">
+    <div id="sme-popup-show-settings" class="settings-section">
+        <div class="setting-row">
+            <input type="radio" name="<?php echo SME_POPUP_SHOW_SETTING; ?>" value="0" <?php checked(get_option(SME_POPUP_SHOW_SETTING), 0); ?>>Disabled<br>
+        </div>
+        <div class="setting-row">
+            <input type="radio" name="<?php echo SME_POPUP_SHOW_SETTING; ?>" value="1" <?php checked(get_option(SME_POPUP_SHOW_SETTING), 1); ?>>At the end of the posts
+        </div>
     </div>
 </div>
 
@@ -36,34 +34,7 @@ final class SocialMediaEverywherePopupSettings implements Settings
 
     public function registerSettings()
     {
-        add_option(SME_TWITTER_ACCOUNT, 'https://');
-        register_setting(SME_OPTIONS_GROUP, SME_TWITTER_ACCOUNT, array(
-            'sanitize_callback' => array($this, 'handleTwitterAccountChange')
-        ));
-        add_option(SME_LINKEDIN_ACCOUNT, 'https://');
-        register_setting(SME_OPTIONS_GROUP, SME_LINKEDIN_ACCOUNT, array(
-            'sanitize_callback' => array($this, 'handleLinkedInAccountChange')
-        ));
-        
         add_option(SME_POPUP_SHOW_SETTING, '0');
         register_setting(SME_OPTIONS_GROUP, SME_POPUP_SHOW_SETTING);
-    }
-
-    private function sanitizeURL($value) {
-        if (empty($value)) {
-            return 'https://';
-        }
-        if (substr($value, 0, 4) !== 'http') {
-            return 'https://' . $value;
-        }
-        return $value;
-    }
-
-    public function handleTwitterAccountChange($value) {
-        return $this->sanitizeURL($value);
-    }
-
-    public function handleLinkedInAccountChange($value) {
-        return $this->sanitizeURL($value);
     }
 }
